acf1a0f7c61d98d19d55026d04e75b7cad79bb15
Holland Naftali <naftaliholland01@gmail.com>
2024-11-12 12:06:28 +0300

    feat: add metrix values

--- frontend/BioSense/src/components/metricCard.jsx
+++ frontend/BioSense/src/components/metricCard.jsx
@@ -6,7 +6,6 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import Guage from '@/components/guage'
 import { Progress } from '@/components/ui/progress'
 
 const MetricCard = ({ metric }) => {
@@ -17,6 +16,7 @@ const MetricCard = ({ metric }) => {
        </CardHeader>
       <CardContent>
         <Progress value={((metric.value - metric.min) / (metric.max - metric.min) * 100) } indicatorColor={metric.color} />
+      <p>{ metric.value } { metric.unit }</p>
       </CardContent>
     </Card>
   );
